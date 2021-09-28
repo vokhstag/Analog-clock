46b7d4c9f3b83614ea63aa728eaf08076f20df33
Yunus <vokhstag@gmail.com>
2021-09-28 06:32:34 +0300

    Initial Commit

---
+++ ClockTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ClockTest
+//
+//  Created by Yunus Abubakarov on 28.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
